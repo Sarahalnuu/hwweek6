4b08149f1cff8555204b1675363b44e58b5dc1eb
Sarahalnuu <Saraotb4400@gmail.com>
2022-11-19 23:55:28 +0300

    homeworkk

--- dist/app.js
+++ dist/app.js
@@ -5,4 +5,73 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.listen(5000);
+let peop = [];
+app.get('/people', (req, res, next) => {
+    return res.json(peop);
+});
+app.post('/people', (req, res) => {
+    const newpeop = req.body;
+    peop.push(newpeop);
+    return res.json({
+        name: "nme add",
+    });
+});
+app.put('/people', (req, res) => {
+    const updatedname = req.body;
+    const id = req.params;
+    const updatenamelist = peop.filter((people) => {
+        return people.id === id;
+    });
+    updatenamelist.push(updatedname);
+    peop = updatenamelist;
+    return res.json({
+        message: 'names updated',
+    });
+});
+app.delete('/people', (req, res, next) => {
+    const id = req.params;
+    const delname = peop.filter(people => {
+        people.id === id;
+    });
+    peop = delname;
+    return res.json({
+        message: 'name deleted',
+    });
+});
+//
+let gradee = [];
+app.get('/grade', (req, res, next) => {
+    return res.json(gradee);
+});
+app.post('/grade', (req, res) => {
+    const newgrade = req.body;
+    gradee.push(newgrade);
+    return res.json({
+        grade: "grade added",
+    });
+});
+app.put('/grade/:id', (req, res) => {
+    const updategrade = req.body;
+    const { id } = req.params;
+    const updategradelist = gradee.filter((grades) => {
+        return grades.id === id;
+    });
+    updategradelist.push(updategrade);
+    gradee = updategradelist;
+    return res.json({
+        message: 'grades updated !',
+    });
+});
+app.delete('/grade/:id', (req, res) => {
+    const { id } = req.params;
+    const delgrade = gradee.filter(grades => {
+        grades.id !== id;
+    });
+    gradee = delgrade;
+    return res.json({
+        message: 'grade deleted ',
+    });
+});
+app.listen(5000, () => {
+    console.log('Server is running in port' + 5000);
+});
